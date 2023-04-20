11eb35075ee4537888c09dbf33654ad1e76be146
Buddy <95647025+BuddyBuie@users.noreply.github.com>
2023-04-20 10:36:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function search(event) {
 }
 
 function init() {
-  fetchData('https://raw.githubusercontent.com/BuddyBuie/Formulary-Json/main/Formulary%204-20-23.JSON')
+  fetchData('https://raw.githubusercontent.com/BuddyBuie/Formulary-Json/main/Formulary%204-20-23.json')
     .then(data => {
       allData = data;
       renderTable(allData, 0, ROWS_PER_PAGE);
